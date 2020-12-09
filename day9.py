a3a8d2b22ce7f4fc592dd339d747343682008048
Sam Saks-Fithian <sam.saks-fithian@blueprintprep.com>
2020-12-09 00:03:08 -0800

    day 9

---
+++ day9.py
@@ -0,0 +1,54 @@
+from itertools import combinations
+
+inputFile = open("day9_input.txt", "r")
+preambleSize = 25
+# inputFile = open("day9_exampleinput.txt", "r")
+# preambleSize = 5
+
+numList = []
+for numStr in inputFile.read().split():
+    numList.append(int(numStr))
+
+# Part 1
+preambleNums = numList[:preambleSize]
+firstInvalid = None
+for index in range(preambleSize, len(numList)):
+    checkNum = numList[index]
+    foundSum = False
+    for pair in combinations(preambleNums, 2):
+        if checkNum == sum(pair):
+            foundSum = True
+            break
+    if not foundSum:
+        # print("invalid num: {}".format(checkNum))
+        firstInvalid = checkNum
+        break
+    preambleNums.pop(0)
+    preambleNums.append(numList[index])
+
+print("Part 1: first invalid number = {}".format(firstInvalid))
+
+# Part 2
+def findContigSum(targetSum, startIndex):
+    contigRange = []
+    found = False
+    for index in range(startIndex, len(numList)):
+        contigRange.append(numList[index])
+        if sum(contigRange) == targetSum and index != startIndex:
+            found = True
+            break
+        if sum(contigRange) > targetSum:
+            break
+    return [found, contigRange]
+
+
+weakness = None
+for startIndex in range(len(numList)):
+    found, contigRange = findContigSum(firstInvalid, startIndex)
+    if found:
+        weakness = min(contigRange) + max(contigRange)
+        break
+
+print("Part 2: encryption weakness = {}".format(weakness))
+
+inputFile.close()
