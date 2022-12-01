321712e519018462ac623a64b051933c7f9d1594
Sam Murray <samsaksfithian@gmail.com>
2022-11-30 22:21:43 -0800

    day1 description cleanup, day2 readme template

---
+++ 2022/Day1/day1.py
@@ -0,0 +1,16 @@
+# from itertools import combinations
+# from pathlib import Path
+
+with open("day1_input.txt", "r") as inputFile:
+    elfPacks = inputFile.read().split("\n\n")
+
+
+print(elfPacks[0])
+# with open('the-zen-of-python.txt') as f:
+#     [print(line) for line in f.readlines()]
+# allWarmUps = Path("warmups.txt").read_text().split("\n\n")
+
+
+# Part 1
+
+# Part 2
---
+++ 2022/Day2/README.md
@@ -0,0 +1,16 @@
+# Day 2: TODO: ???
+
+[Advent of Code website page (original source)](https://adventofcode.com/2022/day/2)
+[Pared down original HTML page](Day2_AdventofCode2022.html)
+
+---
+
+## Part 1
+
+<!-- TODO: -->
+
+---
+
+## Part 2
+
+<!-- TODO: -->
